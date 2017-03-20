78a142974921d62569ef5764dde4966bcbc7091f
YOUR NAME <mariepascalemdjo2@gmail.com>
2017-03-20 12:42:47 +0100

    rendu

--- StPatrickApp-build/StPatrickApp-code/src/main/java/ch/heigvd/res/stpatrick/Application.java
+++ StPatrickApp-build/StPatrickApp-code/src/main/java/ch/heigvd/res/stpatrick/Application.java
@@ -1,5 +1,10 @@
 package ch.heigvd.res.stpatrick;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
 
 /**
  * 
@@ -11,6 +16,18 @@ public class Application {
   
   public IStreamProcessorsFactory getStreamProcessorsFactory() {
     return processorsFactory;
+  }
+  public void process(Reader in, Writer out) throws IOException{
+      BufferedReader br = new BufferedReader(in);
+      BufferedWriter bw = new BufferedWriter(out);
+      int c = br.read();
+      while (c != -1) {
+         c = br.read();
+         out.write(c);
+      }
+      in.close();
+      bw.flush();
+      out.close(); 
   }
 
 }
--- StPatrickApp-build/StPatrickApp-code/src/main/java/ch/heigvd/res/stpatrick/StreamProcessorsFactory.java
+++ StPatrickApp-build/StPatrickApp-code/src/main/java/ch/heigvd/res/stpatrick/StreamProcessorsFactory.java
@@ -14,7 +14,19 @@ public class StreamProcessorsFactory implements IStreamProcessorsFactory {
 
   @Override
   public IStreamProcessor getProcessor(String processorName) throws UnknownNameException {
-    throw new UnknownNameException("The factory does not know any processor called " + processorName);
+    //throw new UnknownNameException("The factory does not know any processor called " + processorName);
+    IStreamProcessor iprocessor = new BasicStreamProcessor();
+     String[] delimiters = {"E", "e"};
+      /*Result extended*/
+      String tokens = new String();
+      /*There is at least one separator*/
+      for (String delim : delimiters) {
+         int index = processorName.indexOf(delim);
+         if (index != -1) {
+            tokens = processorName.substring(index + 1); 
+         }
+      }
+      return iprocessor;
   }
 
 }
